0a691fb50f01dc947c9fc7087302a7b171e2ae32
lekieuanh <anh.le@ntq-solution.com.vn>
2016-07-05 09:30:33 +0700

    onSaveInstanceStateDemo
    This is example for Demo onSaveInstanceState()

--- Ex1Application2/app/src/main/java/com/example/anhlk/ex1application/MainActivity.java
+++ Ex1Application2/app/src/main/java/com/example/anhlk/ex1application/MainActivity.java
@@ -10,14 +10,25 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     static final int request_code = 1;
+    EditText username;
+    EditText age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState!=null){
+            String txt_username = savedInstanceState.getString("txt_username");
+                EditText name = (EditText) findViewById(R.id.username);
+                name.setText(txt_username);
+            Integer txt_age = savedInstanceState.getInt("txt_age");
+                EditText age = (EditText) findViewById(R.id.age);
+                age.setText(txt_age.toString());
+        }
     }
     public void onClick(View view){
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText age = (EditText) findViewById(R.id.age);
+        username = (EditText) findViewById(R.id.username);
+        age = (EditText) findViewById(R.id.age);
 
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra("username", username.getText().toString());
@@ -31,5 +42,19 @@ public class MainActivity extends Activity {
         if(requestCode == request_code && resultCode == RESULT_OK){
             Toast.makeText(this, data.getStringExtra("str"),Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        username = (EditText) findViewById(R.id.username);
+        String txt_username = username.getText().toString();
+
+        age = (EditText) findViewById(R.id.age);
+        Integer txt_age = Integer.parseInt(age.getText().toString());
+
+        outState.putString("txt_username",txt_username);
+        outState.putInt("txt_age", txt_age);
+
+        super.onSaveInstanceState(outState);
     }
 }
